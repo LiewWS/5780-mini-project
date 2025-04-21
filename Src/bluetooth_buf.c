a28178a84f96d2077b4b4eda967c55bea9abd41f
Wei Siew <liewws98@gmail.com>
2025-04-20 19:15:17 -0600

    use separate file for bluetooth buffer

---
+++ Src/bluetooth_buf.c
@@ -0,0 +1,23 @@
+uint8_t recv_buf[BUF_SIZE];
+uint8_t buf_head;
+uint8_t buf_tail;
+
+void USART1_IRQHandler(void)
+{
+    recv_buf[buf_head] = (char)USART1->RDR;
+    inc_idx(&buf_head);
+}
+
+void inc_idx(uint8_t *idx)
+{
+    uint8_t cur_idx = *idx;
+    if (cur_idx >= BUF_SIZE)
+    {
+        *idx = 0;
+    }
+    else
+    {
+        *idx = cur_idx + 1;
+    }
+}
+
