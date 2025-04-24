d3cf1ccfc01b4f06ac6fdf1f9aff1b500ade74ce
Wei Siew <liewws98@gmail.com>
2025-04-23 21:40:07 -0600

    send pwm control instead of i2c reading

--- Src/bluetooth_buf.c
+++ Src/bluetooth_buf.c
@@ -21,9 +21,9 @@ void inc_idx(uint8_t *idx)
     NVIC_EnableIRQ(USART1_IRQn);
 }
 
-void USART1_IRQHandler(void)
-{
-    recv_buf[buf_head] = (char)USART1->RDR;
-    inc_idx(&buf_head);
-}
+// void USART1_IRQHandler(void)
+// {
+//     recv_buf[buf_head] = (char)USART1->RDR;
+//     inc_idx(&buf_head);
+// }
 
--- Src/project_main.c
+++ Src/project_main.c
@@ -1,16 +1,13 @@
 #include "main.h"
 #include "magnetic_encoder.h"
 #include "hal_usart.h"
-#include "bluetooth_buf.h"
 #include "motor.h"
 
-// Incremented by systick interrupt
-extern uint32_t tick_count;
-#define MAX_TICK_COUNT 0xFFFFFFFF
-
+void send_motor_ctrl(void);
 void calibration_loop();
 void balance_loop();
 
+#define MAX_TICK_COUNT 65432
 uint16_t initial_angle;
 
 // Here we assume that angle = 2048 when hanging down
@@ -22,30 +19,23 @@ uint16_t initial_angle;
 
 int project_main()
 {
-    HAL_Init();
-    SystemClock_Config();
+    // HAL_Init();
+    // SystemClock_Config();
 
 #if defined(SENDER)
     // I2C: 
     // Bluetooth RXD: PA9, TXD: PA10
-    send_main();
+    send_motor_ctrl_main();
 #else
     // Enable clock to peripherals
     HAL_RCC_GPIOA_CLK_ENABLE();
     HAL_RCC_GPIOC_CLK_ENABLE();
     HAL_RCC_USART1_CLK_ENABLE();
 
-#if defined(DEBUG)
     // init LEDs in case needed for debugging
     uint16_t ledPins = GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9;
     GPIO_InitTypeDef initLED = {ledPins, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
     HAL_GPIO_Init(GPIOC, &initLED);
-#endif
-
-    // Initialize user button: PA0
-    GPIOA->MODER &= ~(GPIO_MODER_MODER0_0 | GPIO_MODER_MODER0_1);
-    GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR0_0 | GPIO_OSPEEDR_OSPEEDR0_1);
-    GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_1; 
     
     // Configure USART for bluetooth
     configure_TTL_RXint(USART1, HAL_RCC_GetHCLKFreq() / 9600);
@@ -71,21 +61,48 @@ int project_main()
     // Stop motor initially
     pwm_setDutyCycle(0);
 
-    calibration_loop();
-    balance_loop();
+    uint32_t heartbeat = 0;
+
+    while (1) {
+        if (heartbeat == 1000) {
+            HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);
+            heartbeat = 0;
+        }
+        ++heartbeat;
+    }
+
 #endif
 
     return 1;
+}
+
+uint16_t read_i2c_angle(void)
+{
+    uint8_t writtenData[1] = {0x0B};
+    write_i2c(writtenData, MAG_ADDR, 1);
+    uint8_t status = read_i2c(MAG_ADDR);
+    uint16_t angle = 0;
+
+    if (status & 0x20) {
+        writtenData[0] = 0x0C;
+        write_i2c(writtenData, MAG_ADDR, 1);
+        angle = read_i2c(MAG_ADDR) << 8;
+
+        writtenData[0] = 0x0D;
+        write_i2c(writtenData, MAG_ADDR, 1);
+        angle |= read_i2c(MAG_ADDR);
+    }
+
+    return angle;
 }
 
 void calibration_loop()
 {
     uint32_t debouncer = 0;
     uint16_t angle = 0;
-    uint8_t isCalibrating = 1;
-    
-    while (isCalibrating) {
-        angle = read_angle(angle);
+
+    while (1) {
+        angle = read_i2c_angle();
 
         debouncer = (debouncer << 1);
         if(GPIOA->IDR & 1) {
@@ -95,16 +112,12 @@ void calibration_loop()
         if (debouncer == 0x7FFFFFFF) {
             initial_angle = angle;
             pwm_setDutyCycle(50);
-            isCalibrating = 0;
             break;
         }
-    }
-}
 
-void motor_switch_dir()
-{
-    GPIOC->ODR ^= GPIO_ODR_8;
-    GPIOC->ODR ^= GPIO_ODR_9;
+        // Flash red LED
+        GPIOC->ODR ^= (1 << 6);
+    }
 }
 
 typedef enum {
@@ -123,10 +136,6 @@ uint8_t swing_angle_to_pwm(uint16_t cur_angle)
 
 void balance_loop()
 {
-    // Initialize receive buffer
-    buf_head = 0;
-    buf_tail = 0;
-
     uint8_t pwm_dc = 0;
     uint16_t angle = initial_angle;
     uint16_t old_angle = 0;
@@ -138,8 +147,8 @@ void balance_loop()
     balance_state_t bstate = SWING_STATE;
 
     while (1) {
-        angle = read_angle(angle);
-        time = tick_count;
+        angle = read_i2c_angle();
+        time = TIM2->CNT;
         if (old_time > time) {
             // tick_count overflowed
             time_diff = (MAX_TICK_COUNT - old_time) + time;
@@ -151,13 +160,13 @@ void balance_loop()
         }
         anglev = (angle - old_angle) / time_diff;
 
-        bstate = ((angle > SWING_THRES_LEFT) && (angle < SWING_THRES_RIGHT)) ? SWING_STATE : PID_STATE;
+        // bstate = ((angle > SWING_THRES_LEFT) && (angle < SWING_THRES_RIGHT)) ? SWING_STATE : PID_STATE;
         if (bstate == SWING_STATE) {
             if (abs_val(anglev) < abs_val(old_anglev)) {
                 // Passed point of max velocity
                 motor_switch_dir();
-                pwm_dc = swing_angle_to_pwm(angle);
-                pwm_setDutyCycle(pwm_dc);
+                pwm_dc = swing_angle_to_pwm(angle) & 0x7f;
+                USART_send_byte(pwm_dc);
             }
         } else if (bstate == PID_STATE) {
             //
@@ -166,6 +175,65 @@ void balance_loop()
         old_angle = angle;
         old_time = time;
         old_anglev = anglev;
+        // Flash blue LED
+        GPIOC->ODR ^= (1 << 7);
+    }
+}
+
+void send_motor_ctrl(void)
+{
+    // Initialize USART for bluetooth (from checkpoint 1)
+    HAL_RCC_GPIOA_CLK_ENABLE();
+    HAL_RCC_USART1_CLK_ENABLE();
+    HAL_RCC_TIM2_CLK_ENABLE();
+
+    configure_TTL(USART1, HAL_RCC_GetHCLKFreq() / 9600);
+    // USART1 TX Pin (connect to RX of bluetooth)
+    GPIO_InitTypeDef init_pa9 = {GPIO_PIN_9 | GPIO_PIN_10, GPIO_MODE_AF_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL, 1};
+    HAL_GPIO_Init(GPIOA, &init_pa9);
+    // USART1 RX Pin (connect to TX of bluetooth)
+    // GPIO_InitTypeDef init_pa10 = {GPIO_PIN_10, GPIO_MODE_AF_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL, 1};
+    // HAL_GPIO_Init(GPIOA, &init_pa10);
+
+    // Initialize I2C for magnetic encoder (from checkpoint 2)
+    init_i2c();
+
+    // Initialize user button: PA0
+    GPIOA->MODER &= ~(GPIO_MODER_MODER0_0 | GPIO_MODER_MODER0_1);
+    GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR0_0 | GPIO_OSPEEDR_OSPEEDR0_1);
+    GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_1;
+
+    // Initialize timer 2
+    // Upcounting edge aligned
+    TIM2->CR1 &= 0xFFFFFF8F;
+    TIM2->PSC = (0 | 1);
+    TIM2->ARR = (0 | MAX_TICK_COUNT);
+    TIM2->CR1 |= 0x00000001;
+
+    calibration_loop();
+    balance_loop();
+}
+
+void USART1_IRQHandler(void)
+{
+    uint8_t control_byte = (uint8_t) USART1->RDR;
+
+    if (control_byte < 64) {
+        GPIOC->ODR |= (1 << 6);
+    } else {
+        GPIOC->ODR &= ~(1 << 6);
+    }
+    
+    if ((control_byte >= 64) && (control_byte < 128)) {
+        GPIOC->ODR |= (1 << 8);
+    } else {
+        GPIOC->ODR &= ~(1 << 8);
+    }
+
+    if (control_byte >= 128) {
+        GPIOC->ODR |= (1 << 9);
+    } else {
+        GPIOC->ODR &= ~(1 << 9);
     }
 }
 
