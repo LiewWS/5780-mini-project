fde37744dea12068aed6123aff540df424ac47fc
ColinLiechty <colin.liechty@gmail.com>
2025-04-24 13:35:19 -0600

    merged changes

--- Src/project_main.c
+++ Src/project_main.c
@@ -61,7 +61,7 @@ int project_main()
     //HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, 1);
     //HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, 0);
     // Stop motor initially
-    pwm_setDutyCycle(0);
+    pwm_setDutyCycle(0, 0);
 
     uint32_t heartbeat = 0;
 
@@ -113,12 +113,8 @@ void calibration_loop()
 
         if (debouncer == 0x7FFFFFFF) {
             initial_angle = angle;
-<<<<<<< Updated upstream
-            pwm_setDutyCycle(50);
-=======
             pwm_setDutyCycle(50, 0);
-            isCalibrating = 0;
->>>>>>> Stashed changes
+           // isCalibrating = 0;
             break;
         }
 
@@ -193,16 +189,9 @@ void balance_loop()
         if (bstate == SWING_STATE) {
             if (abs_val(anglev) < abs_val(old_anglev)) {
                 // Passed point of max velocity
-<<<<<<< Updated upstream
-                pwm_dc = swing_angle_to_pwm(angle) & 0x7f;
-                // Switch direction
-                pwm_dc |= 0x80;
-                //USART_send_byte(USART1, pwm_dc);
-=======
                 motor_switch_dir();
                 pwm_dc = swing_angle_to_pwm(angle);
                 pwm_setDutyCycle(pwm_dc, 0);
->>>>>>> Stashed changes
             }
         } else if (bstate == PID_STATE) {
             //
